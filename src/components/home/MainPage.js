d2239aa0ac359ed89cf36f8f48596b34345c8d7b
dev01-srivnworks <dev01.srivnworks@gmail.com>
2022-10-09 18:48:21 +0100

    Added useState instead of useRef

--- src/components/home/MainPage.js
+++ src/components/home/MainPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AddNewUser from "../Users/AddNewUser";
+import AddNewUser01 from "../Users/AddNewUser01";
 import UserInfo from "../Users/UserInfo";
 
 const MainPage = () => {
@@ -7,7 +8,8 @@ const MainPage = () => {
   return (
     <>
       <div className="main-panel">
-        <AddNewUser></AddNewUser>
+        <AddNewUser01></AddNewUser01>
+        {/*<AddNewUser></AddNewUser>*/}
         {/*<UserInfo></UserInfo>*/}
       </div>
     </>
